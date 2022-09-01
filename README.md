6dc5af1e3e73bc9708986e18fb23181576515efc
Ethanlinyf <e.yflin@gmail.com>
2022-09-01 11:40:31 +1000

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # TE-Neovim
+## From Vi to Vim, then to Neovim
+Vi is short for “visual editor” and it is available in nearly identical form on every UNIX-like system. 
+## Why Neovim
+## The main features of TE-Neovim
+## Installation
+## Configuration
+## Lisence
+## Contributor
+## Reference
--- lua/basic.lua
+++ lua/basic.lua
@@ -1,11 +1,14 @@
 -- Defaul Coding System
 vim.g.encoding = "UTF-8"
 vim.o.fileencoding = 'utf-8'
+
 -- jk movement 
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
+
 -- line number and use the relative line number
 vim.wo.number = true
--- vim.wo.relativenumber = true
+vim.wo.relativenumber = true
+
 -- highlight line
 vim.wo.cursorline = true
