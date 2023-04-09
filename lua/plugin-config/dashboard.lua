3bd1de9d086c211b1d6d251f6a10fdeb01a72e90
Ethanlinyf <e.yflin@gmail.com>
2023-04-10 09:18:00 +1000

    add doom theme to dashboard

--- lua/plugin-config/dashboard.lua
+++ lua/plugin-config/dashboard.lua
@@ -4,6 +4,7 @@ if not status then
    return
 end
 
+-- theme: hyper
 db.setup({
     theme = 'hyper',
     config = {
@@ -52,3 +53,30 @@ db.setup({
    }
 })
 
+-- doom theme:
+-- db.setup({
+--   theme = 'doom',
+--   config = {
+--     header = {}, --your header
+--     center = {
+--       {
+--         icon = ' ',
+--         icon_hl = 'Title',
+--         desc = 'Find File           ',
+--         desc_hl = 'String',
+--         key = 'b',
+--         keymap = 'SPC f f',
+--         key_hl = 'Number',
+--         action = 'lua print(2)'
+--       },
+--       {
+--         icon = ' ',
+--         desc = 'Find Dotfiles',
+--         key = 'f',
+--         keymap = 'SPC f d',
+--         action = 'lua print(3)'
+--       },
+--     },
+--     footer = {}  --your footer
+--   }
+-- })
