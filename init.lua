710823d5822a4cc49b9332d8d10d1b8e4184a053
Ethanlinyf <e.yflin@gmail.com>
2023-04-09 20:42:19 +1000

    add init.lua, move completely from Vim to NeoVim

---
+++ init.lua
@@ -0,0 +1,33 @@
+
+--[[
+   Copyleft (CL) 2023-2033 Ethan YF Lin
+
+   Something good as indicated, by Dr YF Lin <e.yflin@gmail.com>
+   URL: https://github.com/Ethanlinyf/TE-NeoVim
+   Under ThingsEngine Project: https://www.thethingsengine.org
+   -------------------------------------------------------------------
+   Commentary:
+   TE-NeoVim init configurations 
+   -------------------------------------------------------------------
+--]]
+
+-- basic configurations
+require('basic/static')
+require('basic/dynamic')
+
+-- require('keybindings')
+
+-- require('plugins')
+-- require('plugin-config/nvim-tree')
+-- require('plugin-config/bufferline')
+-- require('plugin-config/telescope')
+-- require('plugin-config/lualine')
+-- require('plugin-config/dashboard')
+-- require("plugin-config/surround")
+-- require("plugin-config/comment")
+
+-- require("lsp/setup")
+-- require("lsp/cmp")
+-- require("lsp/ui")
+
+
