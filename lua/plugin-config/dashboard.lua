d3e3ac1c52673a031379b12c4873759eb7b3629e
Ethanlinyf <e.yflin@gmail.com>
2023-04-11 07:29:15 +1000

    add hyper theme for the startup

--- lua/plugin-config/dashboard.lua
+++ lua/plugin-config/dashboard.lua
@@ -4,79 +4,83 @@ if not status then
    return
 end
 
--- theme: hyper
 db.setup({
-    theme = 'hyper',
-    config = {
-      week_header = {
-       enable = true,
+      theme = 'hyper',
+      config = {
+         week_header = {
+            enable = true,
+         },
+         shortcut = {
+            { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+               icon = ' ',
+               icon_hl = '@variable',
+               desc = 'Files',
+               group = 'Label',
+               action = 'Telescope find_files',
+               key = 'f',
+            },
+            {
+               desc = ' Apps',
+               group = 'DiagnosticHint',
+               action = 'Telescope app',
+               key = 'a',
+            },
+            {
+               desc = ' dotfiles',
+               group = 'Number',
+               action = 'Telescope dotfiles',
+               key = 'd',
+            },
+         },
+         footer = {
+            '',
+            ' 🚀 ThingsEngine -- Do one thing, do it well.',
+         },
       },
-      shortcut = {
-        { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
-        {
-          icon = ' ',
-          icon_hl = '@variable',
-          desc = 'Files',
-          group = 'Label',
-          action = 'Telescope find_files',
-          key = 'f',
-        },
-        {
-          desc = ' Apps',
-          group = 'DiagnosticHint',
-          action = 'Telescope app',
-          key = 'a',
-        },
-        {
-          desc = ' dotfiles',
-          group = 'Number',
-          action = 'Telescope dotfiles',
-          key = 'd',
-        },
-      },
-    },
-  })
-
-db.setup({
-   config = {
-      header = {
-         [[]],
-         [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
-         [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
-         [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
-         [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
-         [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
-         [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-         [[                                                   ]],
-         [[                [ version : 1.0.0 ]                ]],
-      },
-   }
 })
 
--- doom theme:
 -- db.setup({
---   theme = 'doom',
---   config = {
---     header = {}, --your header
---     center = {
---       {
---         icon = ' ',
---         icon_hl = 'Title',
---         desc = 'Find File           ',
---         desc_hl = 'String',
---         key = 'b',
---         keymap = 'SPC f f',
---         key_hl = 'Number',
---         action = 'lua print(2)'
---       },
---       {
---         icon = ' ',
---         desc = 'Find Dotfiles',
---         key = 'f',
---         keymap = 'SPC f d',
---         action = 'lua print(3)'
---       },
---     },
---     footer = {}  --your footer
---   }
+--       theme = "doom",
+--       config = {
+--          header = {
+--             [[]],
+--             [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
+--             [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
+--             [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
+--             [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
+--             [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
+--             [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+--             [[                                                   ]],
+--             [[                [ version : 3.14 ]                 ]],
+--          },
+--          shortcut = {
+--             { desc = '[  Github]', group = 'DashboardShortCut' },
+--             { desc = '[  Ethanlinyf]', group = 'DashboardShortCut' },
+--             { desc = '[  0.1.3]', group = 'DashboardShortCut' },
+--          },
+--          center = {
+--             {
+--                icon = ' ',
+--                icon_hl = 'Title',
+--                desc = 'Find File           ',
+--                desc_hl = 'String',
+--                key = 'b',
+--                keymap = 'SPC f f',
+--                key_hl = 'Number',
+--                action = 'lua print(2)'
+--             },
+--             {
+--                icon = ' ',
+--                desc = 'Find Dotfiles',
+--                key = 'f',
+--                keymap = 'SPC f d',
+--                action = 'lua print(3)'
+--             },
+--          },
+--          footer = {
+--             '',
+--             ' 🚀 ThingsEngine -- Something Good as Indicated',
+--          },
+--       }
 -- })
